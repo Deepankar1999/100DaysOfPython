1abcb59332b43d59cc5d72b4b6cb24398ce37e2d
Deepankar1999 <deepankarsharma.ds99@gmail.com>
2023-07-31 12:36:37 +0530

    first commit

---
+++ day1/band_name_generator.py
@@ -0,0 +1,7 @@
+def nameGenerator():
+    print("Welcome To The Band-Name Generator")
+    city=input("Please Enter The Name Of The City You Grew Up In:-")
+    pet=input("Please Enter Your Pet Name")
+    print("Your Band Name Could be:-"+" "+city+pet)
+
+nameGenerator()
